510ba03c63bb8a36720ba1f5a1b881c53be39b94
Aaron Ryu <aaron.ryu.dev@gmail.com>
2026-08-10 13:52:20 +0900

    7번째: 앞서 만든 Repository 통해 실제 유저가 웹브라우저에서 호출할 상품 조회 API 만들기

--- src/main/java/com/example/demo/controller/internal/api/ProductController.java
+++ src/main/java/com/example/demo/controller/internal/api/ProductController.java
@@ -1,8 +1,16 @@
 package com.example.demo.controller.internal.api;
 
+import com.example.demo.internal.api.dto.ProductResponseDto;
+import com.example.demo.repository.product.Product;
 import com.example.demo.repository.product.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * ProductController
@@ -17,4 +25,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductRepository productRepository;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/internal/api/products")
+    public List<ProductResponseDto> retrieve() {
+        List<Product> products = productRepository.findAll();
+        return products.stream()
+                .map(ProductResponseDto::from)
+                .toList();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/internal/api/products/{id}")
+    public ProductResponseDto retrieve(@PathVariable Integer id) {
+        Optional<Product> wrappedProduct = productRepository.findById(id);
+                 Product         product = wrappedProduct
+                         .orElseThrow(() -> new RuntimeException("찾으시는 유저가 존재하지 않습니다"));
+        return ProductResponseDto.from(product);
+    }
 }
