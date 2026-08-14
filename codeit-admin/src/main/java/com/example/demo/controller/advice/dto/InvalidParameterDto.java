b6eb6dc1af550d03bf6829e83d94bdd55e5d98b1
Aaron Ryu <aaron.ryu.dev@gmail.com>
2026-08-14 16:31:03 +0900

    30번째: 3가지의 메서드 파라미터(혹은 Controller 요청 파라미터)에 대한 검증 실패의 상세 내용을 일관된 DTO 객체의 로그로 파악하기 쉽게 만듬

---
+++ codeit-admin/src/main/java/com/example/demo/controller/advice/dto/InvalidParameterDto.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller.advice.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@ToString
+@Getter
+@Builder
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class InvalidParameterDto {
+    String parameter;           // id
+    Object actualValue;         // 0
+    Object criteriaValue;       // 1
+    String criteria;            // Max
+    String violationMessage;    // 1 이상의 값이 들어가야합니다
+}
+
